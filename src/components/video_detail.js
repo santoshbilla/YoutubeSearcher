f733212603059bf2c6ef1abd932e8d9df628fc33
santoshbilla <santoshbilla@users.noreply.github.com>
2018-06-29 12:14:20 -0700

    Update video_detail.js

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -14,7 +14,7 @@ const VideoDetail = ({video}) => {
       </div>
       <div className='details'>
         <div> {video.snippet.title}</div>
-         <!-- <div> {video.snippet.description}</div> -->
+         {/*  <div> {video.snippet.description}</div> */}
       </div>
     </div>
   )
